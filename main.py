70c57f6e29e3efc7a97fddeddd1b30e82d9fce91
Iza Dusza <izuu12@gmail.com>
2020-05-29 10:34:24 +0200

    creaate a dir for videos (runnin on Windows fixed)

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from data_downloader import download_file_from_google_drive as download
 import SOT_openCV
+import os
+
+def create_dir(dir: str):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 videos = {
     "1ObQ1515WzvgusfJOuh7RVs7Bxxesy8kz": "./data/race.mp4",
-    "1WU8Xqne-6YcusHtQ9sr2qICrB1K4ysGF": "./data/pieski.mp4",
-    "114QEzVWm9FUiy-TAeW0rpAoxvATWuS8W": "./data/nascar_02.mp4",
-    "1cCJncXIvddvEzr_BWNPq2dIuc2-IzY6z": "./data/nascar_01.mp4",
-    "1icwmORKqh6Lv5GMUN_mzn-IyNg1H4GEM": "./data/drone.mp4",
-    "1-9zNexo0RZA3SPQ0q6334QwCWEWsiLO-": "./data/dashcam_boston.mp4",
-    "1jKDtfacJ9JnPT18Qvio14jYWa6CV9e7j": "./data/american_pharoah.mp4",
+    # "1WU8Xqne-6YcusHtQ9sr2qICrB1K4ysGF": "./data/pieski.mp4",
+    # "114QEzVWm9FUiy-TAeW0rpAoxvATWuS8W": "./data/nascar_02.mp4",
+    # "1cCJncXIvddvEzr_BWNPq2dIuc2-IzY6z": "./data/nascar_01.mp4",
+    # "1icwmORKqh6Lv5GMUN_mzn-IyNg1H4GEM": "./data/drone.mp4",
+    # "1-9zNexo0RZA3SPQ0q6334QwCWEWsiLO-": "./data/dashcam_boston.mp4",
+    # "1jKDtfacJ9JnPT18Qvio14jYWa6CV9e7j": "./data/american_pharoah.mp4",
     "1JViJlwpx4IKukwlcuuEMUjpocBg729GW": "./data/pieski2.mp4"
 }
 
@@ -61,30 +66,32 @@ args = {"video": "data/pieski2.mp4", "tracker": "mil"}
 
 if __name__ == "__main__":
 
+    create_dir("data")
+
     # update the video files
     for file_id, filename in videos.items():
         download(file_id, filename)
 
-    for tracker in trackers:
-        args["tracker"] = tracker
-        print(args)
-        # run tracking for the particular video
-        for name, boundingbox in areas.items():
-            tracker = SOT_openCV.choose_tracker(args)
-            initBB = None
-            vs = SOT_openCV.choose_video(args)
-            fps = None
-
-            SOT_openCV.look_ovr_frames(vs, args, initBB, tracker, boundingbox)
-            SOT_openCV.release_pointer(vs, args)
-
-    # run tracking for the particular video
-    # for name, boundingbox in areas.items():
-    #     args = {"video": "data/pieski2.mp4", "tracker": "mil"}
-    #     tracker = SOT_openCV.choose_tracker(args)
-    #     initBB = None
-    #     vs = SOT_openCV.choose_video(args)
-    #     fps = None
-    #
-    #     SOT_openCV.look_ovr_frames(vs, args, initBB, tracker, boundingbox)
-    #     SOT_openCV.release_pointer(vs, args)
+#     for tracker in trackers:
+#         args["tracker"] = tracker
+#         print(args)
+#         # run tracking for the particular video
+#         for name, boundingbox in areas.items():
+#             tracker = SOT_openCV.choose_tracker(args)
+#             initBB = None
+#             vs = SOT_openCV.choose_video(args)
+#             fps = None
+#
+#             SOT_openCV.look_ovr_frames(vs, args, initBB, tracker, boundingbox)
+#             SOT_openCV.release_pointer(vs, args)
+#
+#     # run tracking for the particular video
+#     # for name, boundingbox in areas.items():
+#     #     args = {"video": "data/pieski2.mp4", "tracker": "mil"}
+#     #     tracker = SOT_openCV.choose_tracker(args)
+#     #     initBB = None
+#     #     vs = SOT_openCV.choose_video(args)
+#     #     fps = None
+#     #
+#     #     SOT_openCV.look_ovr_frames(vs, args, initBB, tracker, boundingbox)
+#     #     SOT_openCV.release_pointer(vs, args)
